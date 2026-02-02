0c76b2134625a25e32a95885e2dc38b8d6a560a4
YunJilinzan <3231789754a@gmail.com>
2026-02-03 02:53:46 +0800

    login

--- src/views/index/indexView.vue
+++ src/views/index/indexView.vue
@@ -1,7 +1,30 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div id="index">Index</div>
+  <div id="index">
+    <div class="container">
+      <el-container class="main">
+        <el-aside width="255px">Aside</el-aside>
+        <el-container>
+          <el-main>Main</el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+#index {
+  width: 100%;
+  height: 100%;
+  .container {
+    width: 100%;
+    height: 100%;
+    .main {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+</style>
